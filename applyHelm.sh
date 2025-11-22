fb020986fdbd5301207732304c085a5138cec57f
Subhadeep97 <subhadeepsikdar97@gmail.com>
2025-11-23 01:08:35 +0530

    new changes

---
+++ applyHelm.sh
@@ -0,0 +1,5 @@
+ helm upgrade --install mongo kubernetes/helm/charts/mongo -n  Subhadeep
+ helm upgrade --install backend kubernetes/helm/charts/backend -n Subhadeep
+ helm upgrade --install frontend kubernetes/helm/charts/frontend -n Subhadeep
+ helm upgrade --install admin kubernetes/helm/charts/admin -n Subhadeep
+
---
+++ uninstallHelm.sh
@@ -0,0 +1,5 @@
+ helm  uninstall mongo  -n  Subhadeep
+ helm  uninstall backend -n Subhadeep
+ helm  uninstall frontend  -n Subhadeep
+ helm  uninstall admin -n Subhadeep
+
